d5523218458229fda716c086477b66e75ed703cb
Markus Opolka <markus.opolka@netways.de>
2023-04-17 11:41:46 +0200

    Add integration tests

--- test_check_sar_perf.py
+++ test_check_sar_perf.py
@@ -10,6 +10,7 @@ from check_sar_perf import check_bin
 from check_sar_perf import commandline
 from check_sar_perf import sort_output,sort_combined_output
 from check_sar_perf import SarNRPE
+from check_sar_perf import main
 
 fixture_sar_cpu = """
 Linux 1.2.3-45-generic (unittest)       10/10/2010      _x86_64_	(1 CPU)
@@ -126,6 +127,48 @@ class SarNRPETest(unittest.TestCase):
         expected = ['tps=0.00', 'rkBs=0.00', 'wkBs=0.00', 'areq-sz=0.00', 'aqu-sz=0.00', 'await=0.00', 'svctm=0.00', 'util=0.00']
 
         self.assertEqual(actual, expected)
+
+class MainTest(unittest.TestCase):
+
+    @mock.patch('builtins.print')
+    @mock.patch('check_sar_perf.check_bin')
+    def test_main_no_sar(self, mock_bin, mock_print):
+        mock_bin.return_value = False
+        args = commandline(['cpu'])
+        actual = main(args)
+
+        expected = 2
+        self.assertEqual(actual, expected)
+
+    @mock.patch('builtins.print')
+    @mock.patch('check_sar_perf.SarNRPE', side_effect=KeyError('BOOM!'))
+    @mock.patch('check_sar_perf.check_bin')
+    def test_main_sar_error(self, mock_bin, mock_sar, mock_print):
+        mock_bin.return_value = True
+
+        args = commandline(['cpu'])
+        actual = main(args)
+        expected = 3
+        self.assertEqual(actual, expected)
+
+        args = commandline(['disk', '-d', 'unittest'])
+        actual = main(args)
+        expected = 3
+        self.assertEqual(actual, expected)
+
+    @mock.patch('builtins.print')
+    @mock.patch('check_sar_perf.SarNRPE')
+    @mock.patch('check_sar_perf.check_bin')
+    def test_main_sar_ok(self, mock_bin, mock_sar, mock_print):
+        mock_bin.return_value = True
+        mock_sar.stats = ['test']
+
+        args = commandline(['cpu'])
+        actual = main(args)
+        expected = 0
+        self.assertEqual(actual, expected)
+
+        mock_print.assert_called_once_with('OK: sar |', '')
 
 if __name__ == '__main__':
     unittest.main()
